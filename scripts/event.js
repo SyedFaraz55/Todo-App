00b6e602ed11cb22ee82c6183f420b19191d494a
syed <syedmohi04@gmail.com>
2019-09-02 16:24:27 +0530

    typewriter.js

--- scripts/event.js
+++ scripts/event.js
@@ -1,1 +1,13 @@
+var app = document.getElementById('header');
 
+var typeWriter = new Typewriter(app,{loop:true});
+
+typeWriter.typeString('Welcome to TypeWriter')
+          .pauseFor(2500)
+          .deleteAll()
+          .typeString("Its working awesome")
+          .pauseFor(2500)
+          .deleteChars(7)
+          .typeString('<strong>amazing</strong>')
+          .pauseFor(2500)
+          .start();
